7264429800b5a5ee4fcb31aac02594c9308c8aa9
lanceencarnado <lance@Lances-MacBook-Air.local>
2015-03-07 12:55:15 -0500

    added Queue header

---
+++ Queue.h
@@ -0,0 +1,30 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "Pirate.h"
+
+class Queue
+{
+    class Node 
+    {
+        friend class Queue;
+        private:
+            Pirate* data;
+            Node*   next;
+            Node*   prev;
+    };
+    
+    public:
+        Queue();
+        Queue(Queue&);
+        void push(Pirate*);
+        void pop();
+        Pirate* front();
+        bool empty();
+        bool find(Pirate*);
+        void remove(Pirate*);
+    private:
+        Node* head;
+};
+
+#endif
