24f0587531ade36695486f0ca7909040583cdb00
Angus-Leung <angusleung@Anguss-MacBook-Pro.local>
2015-03-07 13:09:05 -0500

    created the functions in queue.cc

--- Queue.cc
+++ Queue.cc
@@ -6,4 +6,45 @@
 //
 //
 
-#include <stdio.h>
+
+#include Queue.h
+
+
+//Default constructor
+Queue::Queue(){
+    
+}
+
+//Copy constructor
+Queue::Queue(Queue &aQueue){
+    
+}
+
+//Destructor
+Queue::~Queue(){
+    
+}
+
+void Queue::push(Pirate* aPirate){
+    
+}
+
+void Queue::pop(){
+    
+}
+
+Pirate* Queue::front(){
+    
+}
+
+bool Queue::empty(){
+    
+}
+
+bool Queue::find(pirate *aPirate){
+    
+}
+
+void Queue::remove(pirate *aPirate){
+    
+}
